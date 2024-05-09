d5b99dfa6bbf44433bcfc1c796be268ccd4b296e
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-09 11:06:06 +0200

    First commit

---
+++ update.py
@@ -0,0 +1,41 @@
+from subprocess import run
+
+import httpx
+
+URL = "https://api.github.com/repos/prettier/prettier/releases/latest"
+HOOKFILE = "pre-commit-hooks.yaml"
+
+
+def main():
+    resp = httpx.get(URL).raise_for_status()
+    version = resp.json()["tag_name"]
+
+    hook = f"""\
+    - id: prettier
+      name: prettier
+      description: ""
+      entry: prettier --write --ignore-unknown
+      language: node
+      types: [text]
+      args: []
+      require_serial: false
+      additional_dependencies: ["prettier@{version}"]
+      minimum_pre_commit_version: "0"
+    """
+
+    with open(HOOKFILE) as f:
+        cur_hook = f.read()
+
+    if hook == cur_hook:
+        return
+
+    with open(HOOKFILE, "w") as f:
+        f.write(hook)
+
+    run(["git", "add", HOOKFILE], check=True)
+    run(["git", "commit", "-m", f"Update to version {version}"], check=True)
+    run(["git", "tag", f"v{version}"], check=True)
+
+
+if __name__ == "__main__":
+    main()
