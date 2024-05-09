d94a8f8affa81fe8bbffef4ab47305444fdc5507
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-09 21:17:13 +0200

    Automatic update README.md

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+import re
 from subprocess import run
 from textwrap import dedent
 
@@ -5,6 +6,7 @@ import httpx
 
 URL = "https://registry.npmjs.org/prettier/latest"
 HOOKFILE = ".pre-commit-hooks.yaml"
+READMEFILE = "README.md"
 
 
 def main():
@@ -34,7 +36,15 @@ def main():
     with open(HOOKFILE, "w") as f:
         f.write(hook)
 
-    run(["git", "add", HOOKFILE], check=True)
+    with open(READMEFILE) as f:
+        readme = f.read()
+
+    readme = re.sub("rev: v.*", f"rev: v{version}", readme)
+
+    with open(READMEFILE, "w") as f:
+        f.write(readme)
+
+    run(["git", "add", HOOKFILE, READMEFILE], check=True)
     run(["git", "commit", "-m", f"Update to version {version}"], check=True)
     run(["git", "tag", f"v{version}"], check=True)
     run(["git", "push", "origin"], check=True)
